318879e9ad2e00c8b90f512211470cc78b721df6
door48 <161000635+door48@users.noreply.github.com>
2024-03-09 08:50:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-   botToken: "6365253525:AAHE50sSuPWPh7GjmVhdbMOSUP4xJaWIuNw", // Modify your bot TOKEN here
-   hostURL: "https://doak.onrender.com", // Modify your host URL here
+   botToken: "", // Modify your bot TOKEN here
+   hostURL: ".com", // Modify your host URL here
 };
 // for port forewording use "ssh -R 80:localhost:5000 serveo.net"
