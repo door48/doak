606eeaa9d642ed1834da89de7d9dbcd15774a849
door48 <161000635+door48@users.noreply.github.com>
2024-03-09 08:51:27 +0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-module.exports = {
-   botToken: "", // Modify your bot TOKEN here
-   hostURL: ".com", // Modify your host URL here
-};
-// for port forewording use "ssh -R 80:localhost:5000 serveo.net"
